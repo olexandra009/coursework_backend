d355365ea31d274cedfb74aaa1f6e29aafca8f66
Alexandra <a.r.chumachenko@gmail.com>
2020-12-28 23:10:46 +0200

    Change Organization entity configuration

--- DataBaseEntityFramework/ModelConfiguration/OrganizationEntityConfiguration.cs
+++ DataBaseEntityFramework/ModelConfiguration/OrganizationEntityConfiguration.cs
@@ -1,23 +1,35 @@
-﻿using System.Data.Entity.ModelConfiguration;
+﻿//using System.Data.Entity.ModelConfiguration;
 using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.ModelConfiguration
 {
-    class OrganizationEntityConfiguration : EntityTypeConfiguration<Organization>
+    class OrganizationEntityConfiguration : IEntityTypeConfiguration<Organization>
     {
-        public OrganizationEntityConfiguration()
-        {
-            ToTable("Organization");
-            HasKey(u => u.Id);
-            Property(u => u.Id).HasColumnName("Id").IsRequired();
-            Property(u => u.Name).HasColumnName("Name").IsRequired();
-            Property(u => u.Address).HasColumnName("SecondName").IsOptional();
-            Property(u => u.PhoneNumber).HasColumnName("PhoneNumber").IsOptional();
+        //public OrganizationEntityConfiguration()
+        //{
+        //    ToTable("Organization");
+        //    HasKey(u => u.Id);
+        //    Property(u => u.Id).HasColumnName("Id").IsRequired();
+        //    Property(u => u.Name).HasColumnName("Name").IsRequired();
+        //    Property(u => u.Address).HasColumnName("SecondName").IsOptional();
+        //    Property(u => u.PhoneNumber).HasColumnName("PhoneNumber").IsOptional();
            
-            HasMany(o=>o.Users)
-                .WithOptional(u=>u.UserOrganization)
-                .HasForeignKey(u=>u.UserOrganizationId)
-                .WillCascadeOnDelete(false);
+            
+        //}
+
+        public void Configure(EntityTypeBuilder<Organization> builder)
+        {
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Id).IsRequired();
+            builder.Property(u => u.Name).IsRequired();
+            builder.Property(u => u.Address);
+            builder.Property(u => u.PhoneNumber);
+            builder.HasMany(o => o.Users)
+                .WithOne(u => u.UserOrganization)
+                .HasForeignKey(u => u.UserOrganizationId)
+                .OnDelete(DeleteBehavior.NoAction);
         }
     }
 }
