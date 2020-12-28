f47a6a65f999e5417b8f865c30893c32da654dc5
Alexandra <a.r.chumachenko@gmail.com>
2020-12-28 23:20:13 +0200

    Change Petition entity configuration

--- DataBaseEntityFramework/ModelConfiguration/PetitionEntityConfiguration.cs
+++ DataBaseEntityFramework/ModelConfiguration/PetitionEntityConfiguration.cs
@@ -1,19 +1,29 @@
-﻿using System.Data.Entity.ModelConfiguration;
+﻿//using System.Data.Entity.ModelConfiguration;
 using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.ModelConfiguration
 {
-    class PetitionEntityConfiguration : EntityTypeConfiguration<Petition>
+    class PetitionEntityConfiguration : IEntityTypeConfiguration<Petition>
     {
-        public PetitionEntityConfiguration()
-        {
-            ToTable("Petition");
-            HasKey(n => n.Id);
-            Property(n => n.Header).HasColumnName("Header").IsRequired();
-            Property(n => n.Text).HasColumnName("Text").IsRequired();
-            Property(n => n.StarDate).HasColumnName("Start").IsRequired();
-            Property(n => n.FinishDate).HasColumnName("End").IsRequired();
+        //public PetitionEntityConfiguration()
+        //{
+        //    ToTable("Petition");
+        //    HasKey(n => n.Id);
+        //    Property(n => n.Header).HasColumnName("Header").IsRequired();
+        //    Property(n => n.Text).HasColumnName("Text").IsRequired();
+        //    Property(n => n.StarDate).HasColumnName("Start").IsRequired();
+        //    Property(n => n.FinishDate).HasColumnName("End").IsRequired();
 
+        //}
+        public void Configure(EntityTypeBuilder<Petition> builder)
+        {
+            builder.HasKey(n => n.Id);
+            builder.Property(n => n.Header).IsRequired();
+            builder.Property(n => n.Text).IsRequired();
+            builder.Property(n => n.StarDate).IsRequired();
+            builder.Property(n => n.FinishDate).IsRequired();
         }
     }
 }
