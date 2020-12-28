fa1a4d092af29c58e0e49f3eb454e43208480c82
Alexandra <a.r.chumachenko@gmail.com>
2020-12-28 22:59:11 +0200

    Change EntityFRamework and two configuration

--- DataBaseEntityFramework/ModelConfiguration/ApplicationEntityConfiguration.cs
+++ DataBaseEntityFramework/ModelConfiguration/ApplicationEntityConfiguration.cs
@@ -1,21 +1,33 @@
-﻿using System.Data.Entity.ModelConfiguration;
+﻿
 using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.ModelConfiguration
 {
-    class ApplicationEntityConfiguration : EntityTypeConfiguration<Application>
+    class ApplicationEntityConfiguration : IEntityTypeConfiguration<Application>
     {
-        public ApplicationEntityConfiguration()
+        //public ApplicationEntityConfiguration()
+        //{
+        //    ToTable("Application");
+        //    HasKey(n => n.Id);
+        //    Property(n => n.Subject).HasColumnName("Subject").IsRequired();
+        //    Property(n => n.Text).HasColumnName("Text").IsRequired();
+        //    Property(n => n.OpenDate).HasColumnName("Created").IsRequired();
+        //    Property(n => n.CloseDate).HasColumnName("Closed").IsOptional();
+        //    Property(n => n.Status).HasColumnName("Status").IsRequired();
+        //    Property(n => n.Result).HasColumnName("Result").IsOptional();
+        //}
+
+        public void Configure(EntityTypeBuilder<Application> builder)
         {
-            ToTable("Application");
-            HasKey(n => n.Id);
-            Property(n => n.Subject).HasColumnName("Subject").IsRequired();
-            Property(n => n.Text).HasColumnName("Text").IsRequired();
-            Property(n => n.OpenDate).HasColumnName("Created").IsRequired();
-            Property(n => n.CloseDate).HasColumnName("Closed").IsOptional();
-            Property(n => n.Status).HasColumnName("Status").IsRequired();
-            Property(n => n.Result).HasColumnName("Result").IsOptional();
-        
+            builder.HasKey(a => a.Id);
+            builder.Property(n => n.Subject).IsRequired();
+            builder.Property(n => n.Text).IsRequired();
+            builder.Property(n => n.OpenDate).IsRequired();
+            builder.Property(n => n.CloseDate);
+            builder.Property(n => n.Status).IsRequired();
+            builder.Property(n => n.Result);
         }
     }
 }
