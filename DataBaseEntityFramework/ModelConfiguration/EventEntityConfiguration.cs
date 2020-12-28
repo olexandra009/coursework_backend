34b1df83eabf2c887b69b6fbde9361aedaf5daad
Alexandra <a.r.chumachenko@gmail.com>
2020-12-28 23:01:03 +0200

    Change Event and News entity configuration

--- DataBaseEntityFramework/ModelConfiguration/EventEntityConfiguration.cs
+++ DataBaseEntityFramework/ModelConfiguration/EventEntityConfiguration.cs
@@ -1,20 +1,33 @@
-﻿using System.Data.Entity.ModelConfiguration;
+﻿//using System.Data.Entity.ModelConfiguration;
 using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.ModelConfiguration
 {
-    class EventEntityConfiguration : EntityTypeConfiguration<Event>
+    class EventEntityConfiguration : IEntityTypeConfiguration<Event>
     {
-        public EventEntityConfiguration()
+        //public EventEntityConfiguration()
+        //{
+        //    ToTable("Event");
+        //    HasKey(n => n.Id);
+        //Property(n => n.Name).HasColumnName("Name").IsRequired();
+        //Property(n => n.Description).HasColumnName("Description").IsRequired();
+        //Property(n => n.Edited).HasColumnName("Edited").IsRequired();
+        //Property(n => n.StartDate).HasColumnName("Start").IsRequired();
+        //Property(n => n.EndDate).HasColumnName("End").IsRequired();
+        //Property(n => n.EmailNotification).HasColumnName("EmailNotification").IsRequired();
+        //}
+
+        public void Configure(EntityTypeBuilder<Event> builder)
         {
-            ToTable("Event");
-            HasKey(n => n.Id);
-            Property(n => n.Name).HasColumnName("Name").IsRequired();
-            Property(n => n.Description).HasColumnName("Description").IsRequired();
-            Property(n => n.Edited).HasColumnName("Edited").IsRequired();
-            Property(n => n.StartDate).HasColumnName("Start").IsRequired();
-            Property(n => n.EndDate).HasColumnName("End").IsRequired();
-            Property(n => n.EmailNotification).HasColumnName("EmailNotification").IsRequired();
+            builder.HasKey(n => n.Id);
+            builder.Property(n => n.Name).IsRequired();
+            builder.Property(n => n.Description).IsRequired();
+            builder.Property(n => n.Edited).IsRequired();
+            builder.Property(n => n.StartDate).IsRequired();
+            builder.Property(n => n.EndDate).IsRequired();
+            builder.Property(n => n.EmailNotification).IsRequired();
         }
     }
 }
--- DataBaseEntityFramework/ModelConfiguration/NewsEntityConfiguration.cs
+++ DataBaseEntityFramework/ModelConfiguration/NewsEntityConfiguration.cs
@@ -1,18 +1,29 @@
-﻿using System.Data.Entity.ModelConfiguration;
+﻿//using System.Data.Entity.ModelConfiguration;
 using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.ModelConfiguration
 {
-    class NewsEntityConfiguration : EntityTypeConfiguration<News>
+    class NewsEntityConfiguration : IEntityTypeConfiguration<News>
     {
-        public NewsEntityConfiguration()
+        //public NewsEntityConfiguration()
+        //{
+        //    ToTable("News");
+        //    HasKey(n => n.Id);
+        //    Property(n => n.Header).HasColumnName("Header").IsRequired();
+        //    Property(n => n.Text).HasColumnName("Text").IsRequired();
+        //    Property(n => n.Edited).HasColumnName("Edited").IsRequired();
+        //    Property(n => n.DateTimeCreation).HasColumnName("Created").IsRequired();
+        //}
+
+        public void Configure(EntityTypeBuilder<News> builder)
         {
-            ToTable("News");
-            HasKey(n => n.Id);
-            Property(n => n.Header).HasColumnName("Header").IsRequired();
-            Property(n => n.Text).HasColumnName("Text").IsRequired();
-            Property(n => n.Edited).HasColumnName("Edited").IsRequired();
-            Property(n => n.DateTimeCreation).HasColumnName("Created").IsRequired();
+            builder.HasKey(n => n.Id);
+            builder.Property(n => n.Header).IsRequired();
+            builder.Property(n => n.Text).IsRequired();
+            builder.Property(n => n.Edited).IsRequired();
+            builder.Property(n => n.DateTimeCreation).IsRequired();
         }
     }
 }
