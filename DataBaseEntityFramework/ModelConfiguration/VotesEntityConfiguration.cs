269ad686b616cb40c0954c221508dbc4698bff26
Alexandra <a.r.chumachenko@gmail.com>
2020-12-29 00:00:58 +0200

    Change context

--- DataBaseEntityFramework/ModelConfiguration/VotesEntityConfiguration.cs
+++ DataBaseEntityFramework/ModelConfiguration/VotesEntityConfiguration.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.ModelConfiguration
 {
-    class VotesEntityConfiguration
+    class VotesEntityConfiguration : IEntityTypeConfiguration<Votes>
     {
+        public void Configure(EntityTypeBuilder<Votes> builder)
+        {
+            builder.HasKey(cs => cs.Id);
+            builder.HasAlternateKey(cs => new { cs.PetitionId, cs.UserId});
+           
+            //many to one Many Petition
+            builder.HasOne(s => s.Petition)
+                .WithMany(c => c.UserVotes)
+                .HasForeignKey(p=>p.PetitionId).OnDelete(DeleteBehavior.NoAction);
+            builder.HasOne(v => v.User).WithMany(u => u.VotedPetitions)
+                .HasForeignKey(v => v.UserId).OnDelete(DeleteBehavior.NoAction);
+        }
     }
 }
--- DataBaseEntityFramework/PlatformDbContext.cs
+++ DataBaseEntityFramework/PlatformDbContext.cs
@@ -1,38 +1,44 @@
 ﻿
-using System.Data.Entity;
 using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.ModelConfiguration;
 using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework
 {
    public class PlatformDbContext : DbContext
    {
-
-       public PlatformDbContext() : base("CommunityInfoPlatformDB")
+       public PlatformDbContext()
        {
-          
        }
 
-       public DbSet<User> Users { get; set; }
-       public DbSet<Rights> Rights { get; set; }
-       public DbSet<Petition> Petitions{ get; set; }
-      
-       public DbSet<Organization> Organizations { get; set; }
-       public DbSet<News> Newses { get; set; }
-       public DbSet<Event> Events { get; set; }
-       public DbSet<Application> Applications { get; set; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+       public PlatformDbContext(DbContextOptions<PlatformDbContext> options)
+           : base(options)
        {
-           modelBuilder.Configurations.Add(new UserEntityConfiguration());
-           modelBuilder.Configurations.Add(new RightsEntityConfiguration());
-           modelBuilder.Configurations.Add(new PetitionEntityConfiguration());
-           modelBuilder.Configurations.Add(new OrganizationEntityConfiguration());
-           modelBuilder.Configurations.Add(new NewsEntityConfiguration());
-           modelBuilder.Configurations.Add(new EventEntityConfiguration());
-           modelBuilder.Configurations.Add(new ApplicationEntityConfiguration());
+       }
+
+       public virtual DbSet<User> Users { get; set; }
+       public virtual DbSet<Rights> Rights { get; set; }
+       public virtual DbSet<Petition> Petitions{ get; set; }
+       public virtual DbSet<Organization> Organizations { get; set; }
+       public virtual DbSet<News> Newses { get; set; }
+       public virtual DbSet<Event> Events { get; set; }
+       public virtual DbSet<Application> Applications { get; set; }
+       public virtual DbSet<Votes> Votes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+       {
+           base.OnModelCreating(modelBuilder);
+           modelBuilder.ApplyConfiguration(new UserEntityConfiguration());
+           modelBuilder.ApplyConfiguration(new RightsEntityConfiguration());
+           modelBuilder.ApplyConfiguration(new PetitionEntityConfiguration());
+           modelBuilder.ApplyConfiguration(new OrganizationEntityConfiguration());
+           modelBuilder.ApplyConfiguration(new NewsEntityConfiguration());
+           modelBuilder.ApplyConfiguration(new EventEntityConfiguration());
+           modelBuilder.ApplyConfiguration(new ApplicationEntityConfiguration());
+           modelBuilder.ApplyConfiguration(new VotesEntityConfiguration());
+
 
         }
-   }
+    }
 }
 
