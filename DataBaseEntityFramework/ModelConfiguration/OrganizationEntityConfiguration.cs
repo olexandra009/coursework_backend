7a07f937350b21d8ee14a9cf9fc8a85606e6a006
Alexandra <a.r.chumachenko@gmail.com>
2020-12-29 16:28:26 +0200

    Fix Organization Configuration

--- DataBaseEntityFramework/ModelConfiguration/OrganizationEntityConfiguration.cs
+++ DataBaseEntityFramework/ModelConfiguration/OrganizationEntityConfiguration.cs
@@ -1,5 +1,4 @@
-﻿//using System.Data.Entity.ModelConfiguration;
-using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+﻿using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -7,25 +6,13 @@ namespace KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.ModelConf
 {
     class OrganizationEntityConfiguration : IEntityTypeConfiguration<Organization>
     {
-        //public OrganizationEntityConfiguration()
-        //{
-        //    ToTable("Organization");
-        //    HasKey(u => u.Id);
-        //    Property(u => u.Id).HasColumnName("Id").IsRequired();
-        //    Property(u => u.Name).HasColumnName("Name").IsRequired();
-        //    Property(u => u.Address).HasColumnName("SecondName").IsOptional();
-        //    Property(u => u.PhoneNumber).HasColumnName("PhoneNumber").IsOptional();
-           
-            
-        //}
-
         public void Configure(EntityTypeBuilder<Organization> builder)
         {
+            builder.ToTable("Organization");
             builder.HasKey(u => u.Id);
-            builder.Property(u => u.Id).IsRequired();
-            builder.Property(u => u.Name).IsRequired();
-            builder.Property(u => u.Address);
-            builder.Property(u => u.PhoneNumber);
+            builder.Property(u => u.Name).HasColumnName("Organization name").IsRequired();
+            builder.Property(u => u.Address).HasColumnName("Address").IsRequired(false);
+            builder.Property(u => u.PhoneNumber).HasColumnName("PhoneNumber").IsRequired(false);
             builder.HasMany(o => o.Users)
                 .WithOne(u => u.UserOrganization)
                 .HasForeignKey(u => u.UserOrganizationId)
