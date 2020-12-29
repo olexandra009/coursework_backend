3e1140085780ca0e348a5c2a3d6ac06edbbbb614
Alexandra <a.r.chumachenko@gmail.com>
2020-12-29 16:29:42 +0200

    Fix Petition Configuration

--- DataBaseEntityFramework/ModelConfiguration/PetitionEntityConfiguration.cs
+++ DataBaseEntityFramework/ModelConfiguration/PetitionEntityConfiguration.cs
@@ -1,5 +1,4 @@
-﻿//using System.Data.Entity.ModelConfiguration;
-using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+﻿using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -7,23 +6,14 @@ namespace KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.ModelConf
 {
     class PetitionEntityConfiguration : IEntityTypeConfiguration<Petition>
     {
-        //public PetitionEntityConfiguration()
-        //{
-        //    ToTable("Petition");
-        //    HasKey(n => n.Id);
-        //    Property(n => n.Header).HasColumnName("Header").IsRequired();
-        //    Property(n => n.Text).HasColumnName("Text").IsRequired();
-        //    Property(n => n.StarDate).HasColumnName("Start").IsRequired();
-        //    Property(n => n.FinishDate).HasColumnName("End").IsRequired();
-
-        //}
-        public void Configure(EntityTypeBuilder<Petition> builder)
+       public void Configure(EntityTypeBuilder<Petition> builder)
         {
+            builder.ToTable("Petition");
             builder.HasKey(n => n.Id);
-            builder.Property(n => n.Header).IsRequired();
-            builder.Property(n => n.Text).IsRequired();
-            builder.Property(n => n.StarDate).IsRequired();
-            builder.Property(n => n.FinishDate).IsRequired();
+            builder.Property(n => n.Header).HasColumnName("Header").IsRequired();
+            builder.Property(n => n.Text).HasColumnName("Text").IsRequired();
+            builder.Property(n => n.StarDate).HasColumnName("Start").IsRequired();
+            builder.Property(n => n.FinishDate).HasColumnName("End").IsRequired();
         }
     }
 }
