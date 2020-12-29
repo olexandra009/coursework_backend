050cfa5ae7aaf2931d6969919b624844d300b8ac
Alexandra <a.r.chumachenko@gmail.com>
2020-12-29 17:17:46 +0200

    Add models for entity Multimedia and relationship with Multimedia

---
+++ DataBaseEntityFramework/Models/Multimedia.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+
+namespace KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models
+{
+    class Multimedia:IDbModel
+    {
+        #region Fields
+
+        private Guid _id;
+        private String _url;
+
+        #endregion
+
+        #region Properties
+
+        public Guid Id
+        {
+            get => _id;
+            private set => _id = value;
+        }
+        public String Url
+        {
+            get => _url;
+            private set => _url = value;
+        }
+        #endregion
+
+        #region Constructors
+
+        public Multimedia(Guid id, string url)
+        {
+            Id = id;
+            Url = url;
+        }
+
+        public Multimedia()
+        {
+            
+        }
+
+        #endregion
+
+    }
+}
