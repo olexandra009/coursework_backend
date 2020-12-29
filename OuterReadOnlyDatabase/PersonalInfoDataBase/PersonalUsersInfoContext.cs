8c57418909833051baadece8d2966072214313ad
Alexandra <a.r.chumachenko@gmail.com>
2020-12-29 19:06:59 +0200

    Connect to exsisting database to get information only

---
+++ OuterReadOnlyDatabase/PersonalInfoDataBase/PersonalUsersInfoContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace KMA.Coursework.CommunicationPlatform.OuterReadOnlyDatabase.PersonalInfoDataBase
+{
+    public partial class PersonalUsersInfoContext : DbContext
+    {
+        public PersonalUsersInfoContext()
+        {
+        }
+
+        public PersonalUsersInfoContext(DbContextOptions<PersonalUsersInfoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=localhost;port=3306;user=readuser;password=qwerty123;database=PersonalUsersInfo", Microsoft.EntityFrameworkCore.ServerVersion.FromString("8.0.22-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.Property(e => e.UserId).HasColumnName("user_id");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasColumnType("varchar(30)")
+                    .HasColumnName("first_name")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.IpnNumber)
+                    .HasColumnType("varchar(10)")
+                    .HasColumnName("ipn_number")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasColumnType("varchar(30)")
+                    .HasColumnName("last_name")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.PasportNumber)
+                    .IsRequired()
+                    .HasColumnType("varchar(9)")
+                    .HasColumnName("pasport_number")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.SecondName)
+                    .HasColumnType("varchar(30)")
+                    .HasColumnName("second_name")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ OuterReadOnlyDatabase/PersonalInfoDataBase/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace KMA.Coursework.CommunicationPlatform.OuterReadOnlyDatabase.PersonalInfoDataBase
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SecondName { get; set; }
+        public string PasportNumber { get; set; }
+        public string IpnNumber { get; set; }
+    }
+}
