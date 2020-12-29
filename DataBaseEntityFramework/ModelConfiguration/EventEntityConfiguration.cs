66f5904b9246dd1bccb29d6bb1d98a1f9d5edd26
Alexandra <a.r.chumachenko@gmail.com>
2020-12-29 16:20:19 +0200

    Fix Event

--- DataBaseEntityFramework/ModelConfiguration/EventEntityConfiguration.cs
+++ DataBaseEntityFramework/ModelConfiguration/EventEntityConfiguration.cs
@@ -7,27 +7,16 @@ namespace KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.ModelConf
 {
     class EventEntityConfiguration : IEntityTypeConfiguration<Event>
     {
-        //public EventEntityConfiguration()
-        //{
-        //    ToTable("Event");
-        //    HasKey(n => n.Id);
-        //Property(n => n.Name).HasColumnName("Name").IsRequired();
-        //Property(n => n.Description).HasColumnName("Description").IsRequired();
-        //Property(n => n.Edited).HasColumnName("Edited").IsRequired();
-        //Property(n => n.StartDate).HasColumnName("Start").IsRequired();
-        //Property(n => n.EndDate).HasColumnName("End").IsRequired();
-        //Property(n => n.EmailNotification).HasColumnName("EmailNotification").IsRequired();
-        //}
-
         public void Configure(EntityTypeBuilder<Event> builder)
         {
+            builder.ToTable("Event");
             builder.HasKey(n => n.Id);
-            builder.Property(n => n.Name).IsRequired();
-            builder.Property(n => n.Description).IsRequired();
-            builder.Property(n => n.Edited).IsRequired();
-            builder.Property(n => n.StartDate).IsRequired();
-            builder.Property(n => n.EndDate).IsRequired();
-            builder.Property(n => n.EmailNotification).IsRequired();
+            builder.Property(n => n.Name).HasColumnName("EventName").IsRequired();
+            builder.Property(n => n.Description).HasColumnName("Description").IsRequired();
+            builder.Property(n => n.Edited).HasColumnName("Edited").IsRequired();
+            builder.Property(n => n.StartDate).HasColumnName("Start").IsRequired();
+            builder.Property(n => n.EndDate).HasColumnName("End").IsRequired();
+            builder.Property(n => n.EmailNotification).HasColumnName("EmailNotification").IsRequired();
         }
     }
 }
