2f20b8b8b3f6865aa57f588fc9360ff9e4e7c3b4
Alexandra <a.r.chumachenko@gmail.com>
2021-01-02 20:20:17 +0200

    Create abstract class that inherits IRepositoryBase

---
+++ DataBaseEntityFramework/Repositories/EfRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Repositories
+{
+    class EfRepository
+    {
+    }
+}
