819bb44374f9bf246ffec12e2c0551cb45a615b4
Alexandra <a.r.chumachenko@gmail.com>
2021-01-03 15:33:15 +0200

    Add Autofac to ServerHttpPlatform project

--- OuterReadOnlyDatabase/Repositories/RepositoryImplementation.cs
+++ OuterReadOnlyDatabase/Repositories/RepositoryImplementation.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace KMA.Coursework.CommunicationPlatform.OuterReadOnlyDatabase.Repositories
 {
-    class RepositoryImplementation
+    public class RepositoryImplementation <TEntity>: IReadOnlyRepository<TEntity> 
+        where TEntity:class
     {
+        protected readonly DbContext DbContext;
+
+        public RepositoryImplementation(DbContext context)
+        {
+            DbContext = context;
+        }
+
+        public Task<bool> Exists(TEntity entity)
+        {
+            //TODO create method that will get entity and specification maybe
+            //     and return true if there are items with it (get by alternative key)
+            throw new NotImplementedException();
+        }
     }
 }
