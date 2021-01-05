17fc7ff9220b6ef42d28a15782662e70a27c9cf3
Alexandra <a.r.chumachenko@gmail.com>
2021-01-05 16:58:08 +0200

    Add interface for services

---
+++ ServerHttpPlatform/Services/Common/IServiceCRUDModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Services.Common
+{
+    public interface IServiceCrudModel<TModel, TKey>
+    {
+        Task<TModel> Get(TKey id);
+        Task<TModel> Create(TModel model);
+        Task<TModel> Update(TModel model);
+        Task Delete(TKey id);
+        Task<bool> Exist(TKey id);
+
+    }
+}
