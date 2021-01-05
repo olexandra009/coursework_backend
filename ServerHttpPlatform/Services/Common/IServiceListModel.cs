e6a0567b0c1958b491fb6ba6acc2812a784397ff
Alexandra <a.r.chumachenko@gmail.com>
2021-01-05 17:02:22 +0200

    Add interface for services that includes methods working with collection

--- ServerHttpPlatform/Services/Common/IServiceListModel.cs
+++ ServerHttpPlatform/Services/Common/IServiceListModel.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Ardalis.Specification;
 
 namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Services.Common
 {
-    interface IServiceListModel
+    public interface IServiceListModel<TModel, TEntity>
     {
+        Task<List<TModel>> List();
+        //todo: maybe methods with listQuery should be added 
+        Task<List<TModel>> List(ISpecification<TEntity> specification);
+        Task<int> Count(ISpecification<TEntity> specification);
     }
 }
