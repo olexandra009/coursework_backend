db9a545e145e43c8fce2e822cce10aa083abd764
Alexandra <a.r.chumachenko@gmail.com>
2021-01-08 12:58:44 +0200

    Add methods for implement IServiceListModel

--- ServerHttpPlatform/Services/Common/ServiceListModel.cs
+++ ServerHttpPlatform/Services/Common/ServiceListModel.cs
@@ -23,15 +23,35 @@ namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Services.Commo
 
         public virtual async Task<List<TModel>> List()
         {
-            throw new NotImplementedException();
+            List<TEntity> entities = await Repository.ListAsync();
+            List<TModel> models = Mapper.Map<List<TModel>>(entities);
+            return models;
+        }
+
+        public Task<List<TModel>> List(int take, int skip = 0, string sortProp = null, string sortOder = null)
+        {
+            return List(CreateSpecification(take, skip, sortProp, sortOder));
+        }
+
+        public Task<int> Count(int take, int skip = 0, string sortProp = null, string sortOder = null)
+        {
+            return Count(CreateSpecification(take, skip, sortProp, sortOder));
         }
 
         public virtual async Task<List<TModel>> List(ISpecification<TEntity> specification)
         {
-            throw new NotImplementedException();
+            List<TEntity> entities = await Repository.ListAsync(specification);
+            List<TModel> models = Mapper.Map<List<TModel>>(entities);
+            return models;
         }
 
-        public virtual async Task<int> Count(ISpecification<TEntity> specification)
+        public virtual Task<int> Count(ISpecification<TEntity> specification)
+        {
+            return Repository.CountAsync(specification);
+        }
+
+        //todo implement sorted specification and page sorted and CreateSpecification method
+        protected virtual ISpecification<TEntity> CreateSpecification(int take, int skip = 0, string sortProp = null, string sortOder = null)
         {
             throw new NotImplementedException();
         }
