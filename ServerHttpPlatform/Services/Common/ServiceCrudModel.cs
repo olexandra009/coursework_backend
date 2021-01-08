10ae195e11bddbe1d6b17d15859e63457d737515
Alexandra <a.r.chumachenko@gmail.com>
2021-01-08 12:41:14 +0200

    Add templates for services common classes

---
+++ ServerHttpPlatform/Services/Common/ServiceCrudModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Services.Common
+{
+    public class ServiceCrudModel<TModel, TKey, TEntity> : IServiceCrudModel<TModel, TKey>
+    {
+    }
+}
---
+++ ServerHttpPlatform/Services/Common/ServiceListModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ardalis.Specification;
+using AutoMapper;
+using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Repositories.Common;
+
+namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Services.Common
+{
+    public class ServiceListModel<TModel, TEntity> : IServiceListModel<TModel, TEntity>
+    where TEntity: class
+    {
+        protected IMapper Mapper;
+        protected IRepository<TEntity> Repository;
+
+        public ServiceListModel(IMapper mapper, IRepository<TEntity> repository)
+        {
+            Mapper = mapper;
+            Repository = repository;
+        }
+
+
+        public virtual async Task<List<TModel>> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual async Task<List<TModel>> List(ISpecification<TEntity> specification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual async Task<int> Count(ISpecification<TEntity> specification)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
