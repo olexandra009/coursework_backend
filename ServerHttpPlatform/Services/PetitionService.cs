d114ac5c516cf770909732011ea2db58035b4c1d
Alexandra <a.r.chumachenko@gmail.com>
2021-01-11 16:51:06 +0200

    Implement base service for user and petition

--- ServerHttpPlatform/Services/PetitionService.cs
+++ ServerHttpPlatform/Services/PetitionService.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Repositories.Common;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Models;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Services.Common;
 
 namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Services
 {
-    public class PetitionService
+    public interface IPetitionService : IServiceCrudModel<Petition, int, PetitionEntity>
     {
+    }
+    public class PetitionService : ServiceCrudModel<Petition, int, PetitionEntity>, IPetitionService
+    {
+        public PetitionService(IMapper mapper, IRepository<PetitionEntity> repository) : base(mapper, repository)
+        {
+        }
     }
 }
--- ServerHttpPlatform/Services/UserService.cs
+++ ServerHttpPlatform/Services/UserService.cs
@@ -1,11 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
 using System.Threading.Tasks;
+using AutoMapper;
+using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Repositories.Common;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Models;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Services.Common;
 
 namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Services
 {
-    public class UserService
+    //todo implement methods 
+    public interface IUserService : IServiceCrudModel<User,int,UserEntity>
     {
+        Task Registration();
+        Task Login();
+        Task<User> UpdateRole();
+    }
+    public class UserService:ServiceCrudModel<User, int, UserEntity>, IUserService
+    {
+        public UserService(IMapper mapper, IRepository<UserEntity> repository) : base(mapper, repository)
+        {
+        }
+
+        public async Task Registration()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task Login()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<User> UpdateRole()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
