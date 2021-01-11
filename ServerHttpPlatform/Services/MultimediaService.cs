e71e08083c2977aa5858c2b1b194228ed0e3bc4f
Alexandra <a.r.chumachenko@gmail.com>
2021-01-11 16:42:33 +0200

    Implement service for multimedia

--- ServerHttpPlatform/Services/MultimediaService.cs
+++ ServerHttpPlatform/Services/MultimediaService.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Repositories.Common;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Models;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Services.Common;
 
 namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Services
 {
-    public class MultimediaService
+    public interface IMultimediaService : IServiceCrudModel<Multimedia, int, MultimediaEntity>
     {
+    }
+    //todo create specification to get by event/application/news id 
+    public class MultimediaService : ServiceCrudModel<Multimedia, int, MultimediaEntity>
+    {
+        public MultimediaService(IMapper mapper, IRepository<MultimediaEntity> repository) : base(mapper, repository)
+        {
+        }
     }
 }
