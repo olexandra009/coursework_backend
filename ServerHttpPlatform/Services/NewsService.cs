61a7e5badd206b3538466c13a6312f7d732760eb
Alexandra <a.r.chumachenko@gmail.com>
2021-01-11 15:33:39 +0200

    Implement service for news

--- ServerHttpPlatform/Services/NewsService.cs
+++ ServerHttpPlatform/Services/NewsService.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Repositories.Common;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Models;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Services.Common;
 
 namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Services
 {
-    public class NewsService
+    public interface INewsService:IServiceCrudModel<News, int, NewsEntity>
     {
+
+    }
+    public class NewsService : ServiceCrudModel<News, int, NewsEntity>
+    {
+        public NewsService(IMapper mapper, IRepository<NewsEntity> repository) : base(mapper, repository)
+        {
+            
+        }
+
+        public override Task<News> Update(News model)
+        {
+            model.Edited = true;
+            return base.Update(model);
+        }
     }
 }
