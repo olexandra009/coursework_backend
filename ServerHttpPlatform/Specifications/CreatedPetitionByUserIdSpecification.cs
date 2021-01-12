e0a886bf045b557183def951d6b408d38688722a
Alexandra <a.r.chumachenko@gmail.com>
2021-01-12 16:56:25 +0200

    Create CreatedPettionByUserIdSpecification

---
+++ ServerHttpPlatform/Specifications/CreatedPetitionByUserIdSpecification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Specifications.Common;
+
+namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Specifications
+{
+    public class CreatedPetitionByUserIdSpecification : PagedSpecification<PetitionEntity>
+    {
+        public CreatedPetitionByUserIdSpecification(int userId, int take = 10, int skip = 0, string sortProp = null, string sortOrder = null) 
+            : base(take, skip, sortProp, sortOrder)
+        {
+            Query.Where(p => p.AuthorId == userId);
+        }
+    }
+}
--- ServerHttpPlatform/Specifications/VotesForPetitionWithId.cs
+++ ServerHttpPlatform/Specifications/VotesForPetitionWithId.cs
@@ -1,12 +1,13 @@
-﻿using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+﻿using Ardalis.Specification;
+using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
 using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Specifications.Common;
 
 namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Specifications
 {
     
-    public class VotesForPetitionWithId : PagedSpecification<VotesEntity>
+    public class VotesForPetitionWithId : Specification<VotesEntity>
     {
-        public VotesForPetitionWithId(int petitionId, int take = 10, int skip = 0, string sortProp = null, string sortOrder = null) : base(take, skip, sortProp, sortOrder)
+        public VotesForPetitionWithId(int petitionId)
         {
            Query.Where(v => v.PetitionId == petitionId).Include(v=>v.User);
 
