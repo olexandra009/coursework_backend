c03ba5197f8bb9db36060f97ed675ad67ee2a67c
Alexandra <a.r.chumachenko@gmail.com>
2021-01-12 16:35:52 +0200

    Create and test VotesForPetitionWithId

--- ServerHttpPlatform/Program.cs
+++ ServerHttpPlatform/Program.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Ardalis.Specification;
 using Autofac.Extensions.DependencyInjection;
 using AutoMapper;
 using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework;
 using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
 using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Repositories;
+using KMA.Coursework.CommunicationPlatform.OuterReadOnlyDatabase.PersonalInfoDataBase;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Specifications;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,6 +31,20 @@ namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform
                // UserEntity user = use.GetByIdAsync(1).Result;
                
                 PetitionRepository pet = new PetitionRepository(context);
+                VotesRepository uss = new VotesRepository(context);
+                var usl = uss.ListAsync(new UsersThatVotedPetitionWithId(1)).Result;
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine(usl.Count);
+                Console.WriteLine(usl[0].UserId);
+                Console.WriteLine("----------------------------------------");
+
+
+                usl = uss.ListAsync(new UsersThatVotedPetitionWithId(100)).Result;
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine(usl.Count);
+                // Console.WriteLine(usl[0].Login);
+                Console.WriteLine("----------------------------------------");
+
                 //PetitionEntity p = new PetitionEntity();
                 //p.Text = "New test close successful petition";
                 //p.StarDate = new DateTime(2000, 12, 1);
@@ -35,7 +52,7 @@ namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform
                 //p.Header = "Header";
                 //p.Author = user;
                 //p.AuthorId = user.Id;
-              
+
                 //var pp = pet.AddAsync(p).Result;
 
                 //List<PetitionEntity> pett = pet.ListAsync(new FilterStatusPetitionSpecification("close", "unsucc")).Result;
---
+++ ServerHttpPlatform/Specifications/VotesForPetitionWithId.cs
@@ -0,0 +1,15 @@
+﻿using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Specifications.Common;
+
+namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Specifications
+{
+    
+    public class VotesForPetitionWithId : PagedSpecification<VotesEntity>
+    {
+        public VotesForPetitionWithId(int petitionId, int take = 10, int skip = 0, string sortProp = null, string sortOrder = null) : base(take, skip, sortProp, sortOrder)
+        {
+           Query.Where(v => v.PetitionId == petitionId).Include(v=>v.User);
+
+        }
+    }
+}
