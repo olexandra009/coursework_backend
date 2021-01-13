6bd8aa6f9b4f95492668c01318244dc421200431
Alexandra <a.r.chumachenko@gmail.com>
2021-01-13 12:32:38 +0200

    Add two specification for filter application by athor id and by status

---
+++ ServerHttpPlatform/Specifications/ApplicationsByAuthor.cs
@@ -0,0 +1,21 @@
+﻿using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Specifications.Common;
+
+namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Specifications
+{
+    /// <summary>
+    /// Specification query for getting list of application by author id
+    /// </summary>
+    public class ApplicationsByAuthor : PagedSpecification<ApplicationEntity>
+    {
+        /// <summary>
+        /// Creates specification query for getting list of application by author id
+        /// </summary>
+        /// <param name="userId">id of application's author</param>
+        /// <param name="query"></param>
+        public ApplicationsByAuthor(int userId, PagedSortListQuery query) : base(query.Take, query.Skip, query.SortProp, query.SortOrder)
+        {
+            Query.Where(a => a.AuthorId == userId);
+        }
+    }
+}
---
+++ ServerHttpPlatform/Specifications/FilterByStatusApplicationSpecification.cs
@@ -0,0 +1,24 @@
+﻿using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Specifications.Common;
+
+namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Specifications
+{
+    /// <summary>
+    /// Specification query for get list of application filtered by status
+    /// </summary>
+    public class FilterByStatusApplicationSpecification : PagedSpecification<ApplicationEntity>
+    {
+        /// <summary>
+        /// Creates specification query for get list of application filtered by status
+        /// </summary>
+        /// <param name="status"></param>
+        /// <param name="query"></param>
+        public FilterByStatusApplicationSpecification(Status status, PagedSortListQuery query) : base(query.Take, 
+                                                                                                      query.Skip, 
+                                                                                                      query.SortProp, 
+                                                                                                      query.SortOrder)
+        {
+            Query.Where(a => a.Status == status);
+        }
+    }
+}
