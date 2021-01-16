9eb4a33b31ff7f1ca829ce629c2f256669c95c75
Alexandra <a.r.chumachenko@gmail.com>
2021-01-16 12:35:14 +0200

    Add Organization Controller

---
+++ ServerHttpPlatform/Controllers/OrganizationController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Controllers.Common;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.DTO;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Models;
+using KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KMA.Coursework.CommunicationPlatform.ServerHttpPlatform.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganizationController : CrudControllerBase<OrganizationDTO, Organization, OrganizationEntity, int>
+    {
+
+        public OrganizationController(IOrganizationService service, IMapper mapper) : base(service, mapper)
+        {
+        }
+    }
+}
