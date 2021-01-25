fe3fcfba3c496db0eb3abc5a05785e4e35090ea7
Alexandra <a.r.chumachenko@gmail.com>
2021-01-25 17:25:35 +0200

    Add repository for email

---
+++ DataBaseEntityFramework/Repositories/EmailConfirmationRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Models;
+using KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Repositories.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace KMA.Coursework.CommunicationPlatform.DataBaseEntityFramework.Repositories
+{
+    public interface IEmailConfirmationRepository : IRepository<EmailConfirmEntity>
+    {
+
+    }
+    public class EmailConfirmationRepository : EfRepository<EmailConfirmEntity>, IEmailConfirmationRepository
+    {
+        public EmailConfirmationRepository(PlatformDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
